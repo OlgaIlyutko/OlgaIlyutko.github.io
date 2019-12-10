6498d23b3c7a265837bfc214afd7bf6d88e52183
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2019-12-10 10:45:25 +0300

    first input

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-console.log('hei');
+const but1 = document.querySelector('#main');
+const input1 = document.querySelector('#question1');
+but1.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (input1.value === '1') {
+        alert('+');
+    }
+    else {
+        alert('-');
+    }
+})
