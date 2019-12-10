ddea6cb09e913848a2573c8d41faf48674ca5213
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2019-12-10 11:14:43 +0300

    localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,22 @@
+const isStorageSupport = true;
+
 const but1 = document.querySelector('#main');
 const input1 = document.querySelector('#question1');
+
 but1.addEventListener('click', (evt) => {
     evt.preventDefault();
-    if (input1.value === '1') {
-        alert('+');
-    }
-    else {
-        alert('-');
-    }
+
+    localStorage.getItem("but1");
+    if (!localStorage.getItem("but1")) {
+        alert('no push');
+        if (input1.value === '1') {
+            alert('+');
+            localStorage.setItem("but1", true);
+        }
+        else {
+            alert('-');
+        }
+    } else {
+        alert('вы уже правильно отвечали на этот вопрос')
+    } 
 })
